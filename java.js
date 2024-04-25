6412c4bdb1a4625b15e4b221b908e196a967cf70
Alexander <a.carlssons98@gmail.com>
2024-04-25 18:40:24 +0200

    started on the js with querySelectors

---
+++ java.js
@@ -0,0 +1,15 @@
+let calculator = document.querySelector(".calculator");
+let screen = document.querySelector("#screen");
+let values = document.querySelector("#values");
+let previousValue = document.querySelector("#previousValue");
+let currentValue = document.querySelector("#currentValue");
+
+let clear = document.querySelector("#clear");
+let equal = document.querySelector("#equal");
+let decimal = document.querySelector("#decimal");
+let number = document.querySelectorAll(".number");
+let operator = document.querySelectorAll(".operator");
+
+
+
+

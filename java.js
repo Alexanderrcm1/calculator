98e26e97f0496dc0de060b76eeb3997531c0cc5f
Alexander <a.carlssons98@gmail.com>
2024-04-26 06:03:11 +0200

    added event listeners for pressing number and operator buttons, made functions of what happens when you press a button

--- java.js
+++ java.js
@@ -1,14 +1,44 @@
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+
+document.addEventListener("DOMContentLoaded", function() {
 let calculator = document.querySelector(".calculator");
 let screen = document.querySelector("#screen");
-let values = document.querySelector("#values");
-let previousValue = document.querySelector("#previousValue");
-let currentValue = document.querySelector("#currentValue");
+let screenValues = document.querySelector("#screenValues");
+let previousScreen = document.querySelector("#previousScreen");
+let currentScreen = document.querySelector("#currentScreen");
 
 let clear = document.querySelector("#clear");
 let equal = document.querySelector("#equal");
 let decimal = document.querySelector("#decimal");
-let number = document.querySelectorAll(".number");
-let operator = document.querySelectorAll(".operator");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+
+numbers.forEach((number) => number.addEventListener("click", function(e) {
+    handleNumber(e.target.textContent);
+    currentScreen.textContent = currentValue;
+}));
+
+operators.forEach((operator) => operator.addEventListener("click", function (e) {
+    handleOperator(e.target.textContent);
+    previousScreen.textContent = previousValue;
+    currentScreen.textContent = currentValue;
+}));
+
+
+});
+function handleNumber(num) {
+    if (currentValue.length <= 5) {
+        currentValue += num;
+    };
+};
+
+function handleOperator(op) {
+    previousValue = currentValue;
+    currentValue = op;
+}
+
 
 
 
